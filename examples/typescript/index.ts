cc0fdb0a42fed03dac6428506ea1ded7ac1d59a8
Brian Ginsburg <brian@brianginsburg.com>
2019-08-25 09:29:16 -0700

    Upgrade standardized-audio-context
    Update retrigger to use cancelAndHoldAtTime.
    Change to single quotes throughout project.
    Update tests of retrigger.

--- examples/typescript/index.ts
+++ examples/typescript/index.ts
@@ -1,5 +1,5 @@
-import { AudioContext } from 'standardized-audio-context';
-import { Envelope } from './env-gen';
+import { AudioContext } from "standardized-audio-context";
+import { Envelope } from "../../env-gen";
 
 const audioContext = new AudioContext();
 let oscillatorNode = null;
@@ -7,8 +7,7 @@ let oscillatorNode = null;
 const amp = audioContext.createGain();
 amp.gain.setValueAtTime(0.1, audioContext.currentTime);
 
-// const envSettings = { attackTime: 1, decayTime: 1, sustainLevel: 0.5, releaseTime: 1 };
-const envSettings = { attackTime: 2, decayTime: 2, sustainLevel: 0.5, releaseTime: 2 };
+const envSettings = { attackTime: 1, decayTime: 1, sustainLevel: 0.5, releaseTime: 1 };
 // const envSettings = { initialLevel: 0.5, attackTime: 2, decayTime: 2, sustainLevel: 0.3, releaseTime: 3 };
 // const envSettings = { attackTime: 2, attackFinalLevel: 0.3, decayTime: 2, sustainLevel: 0.3, releaseTime: 2 };
 // const envSettings = {
@@ -29,13 +28,13 @@ amp.connect(masterGain);
 ampEnv.connect(amp.gain);
 masterGain.connect(audioContext.destination);
 
-document.getElementById('start').addEventListener('click', () => {
+document.getElementById("start").addEventListener("click", () => {
   if (audioContext.currentTime < ampEnv.getEndTime()) {
     oscillatorNode.stop(audioContext.currentTime + 1000);
-    ampEnv.retrigger(audioContext.currentTime, envSettings);
+    ampEnv.retrigger(audioContext.currentTime, { attackTime: 0.1, decayTime: 2, sustainLevel: 0.2, releaseTime: 3 });
   } else {
     oscillatorNode = audioContext.createOscillator();
-    oscillatorNode.type = 'sine';
+    oscillatorNode.type = "sine";
     oscillatorNode.connect(amp);
 
     const now = audioContext.currentTime;
@@ -45,7 +44,7 @@ document.getElementById('start').addEventListener('click', () => {
   }
 });
 
-document.getElementById('stop').addEventListener('click', () => {
+document.getElementById("stop").addEventListener("click", () => {
   const now = audioContext.currentTime;
   ampEnv.closeGate(now);
 
